7c790a1125661e3632a476d54836767579a60d6b
Pedro Pereira <plp.leoncio@gmail.com>
2022-06-17 11:34:05 -0300

    remake base feita

--- javinha.js
+++ javinha.js
@@ -3,173 +3,13 @@ pontos = 0;
 numeroDeCartas = 0;
 trava = 0;
 stringDeErro = "";
+//////////////////////////
+lista = []
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-function listaAleatoria(numero){
-
-    let lista = []
-
-    for(let i = 0; i < (numero); i++){
-        lista.push(i + 1)
-    }
-
-    lista = lista.sort(comparador);
-    return lista;
-}
-
-function converterLista(lista){
-    let listaConvertida = []
-    for(let i = 0; i < lista.length;i++){
-
-        switch (lista[i]) {
-            case 1:
-                lista[i] = 1;
-                break;
-
-            case 2:
-                lista[i] = 3;
-                break;
-
-            case 3:
-                lista[i] = 5;
-                break;
-
-            case 4:
-                lista[i] = 7;
-                break;
-
-            case 5:
-                lista[i] = 9;
-                break;
-            
-            case 6:
-                lista[i] = 11;
-                break;
-
-            case 7:
-                lista[i] = 13;
-                break;
-
-            case 8:
-                lista[i] = 15;
-                break;
-
-            case 9:
-                lista[i] = 17;
-                break;
-
-            case 10:
-                lista[i] = 19;
-                break;
-
-            case 11:
-                lista[i] = 21;
-                break;
-            
-            case 12:
-                lista[i] = 23;
-                break;
-            default:
-                break;
-        }
-    }
-    return lista;
-
-}
-
-function quantidadeDeCartas(erro){    
-
-    const numero = prompt("Com quantas cartas você deseja jogar ? " + erro);
-
-    if (numero % 2 === 0 && numero <= 12){
-
-        let lista = listaAleatoria(numero);; 
-        cartasNaMesa(lista);
-
-    } else {
-
-        stringDeErro = "\nLembre-se de colocar um número par menor que 12";
-        quantidadeDeCartas(stringDeErro);
-
-    }
-
-}    
-
-function cartasNaMesa(lista){
-
-    numeroDeCartas = lista.length;
-    let localizarA = document.querySelector(".caixa-cartas-A")
-
-    let listaConvertida = converterLista(lista)
-
-    for(let i = 0; i < lista.length; i++){
-        //[ 2 , 3 , 1 , 4]
-        //[ 1 , 3 , 1 , 3]
-        switch (i + 1) {
-
-            /////document.querySelector("#image").setAttribute('src', 'outra_imagem.png')
-
-            case 1:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora") 
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/um.gif')
-                break;             
-            case 2:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")  
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/um.gif') 
-                break;  
-            case 3:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")  
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/dois.gif')
-                break;   
-            case 4:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/dois.gif')  
-                break;   
-            case 5:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")  
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/tres.gif') 
-                break;  
-            case 6: 
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora") 
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/tres.gif')   
-                break; 
-            /////////////////////************************************************************************** */
-            case 7:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora") 
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/quatro.gif')
-                break;             
-            case 8:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")  
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/quatro.gif') 
-                break;  
-            case 9:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")  
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/cinco.gif')
-                break;   
-            case 10:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/cinco.gif')  
-                break;   
-            case 11:
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora")  
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/seis.gif') 
-                break;  
-            case 12: 
-                localizarA.childNodes[listaConvertida[i]].classList.toggle("fora") 
-                localizarA.childNodes[listaConvertida[i]].childNodes[3].setAttribute('src', 'imagens/seis.gif')   
-                break; 
-
-                       
-            default:
-                
-        }
-    }
-}
-
-function fimDeJogo(){}
 function clicou (elemento){
 
     //contador de jogadas++
@@ -257,7 +97,57 @@ function clicou (elemento){
     
 }
 }
-10
-quantidadeDeCartas(stringDeErro)
 
+function clickTeste(elemento){
 
+    console.log(elemento)
+
+    elemento.classList.toggle("giragira")
+    
+    setTimeout(function(){
+        
+        elemento.childNodes[1].classList.toggle("rodou");
+        elemento.childNodes[3].classList.toggle("rodou");
+        elemento.classList.toggle("giragira")
+        
+        }, 500);
+  
+}
+
+function cartasNaMesa(){
+    const quantidade = prompt("Manda")
+    const elemento = document.querySelector(".caixa-cartas")
+
+    for (let i = 0; i < quantidade; i++){
+
+        if (i <= (quantidade - 1 ) / 2){
+        lista.push(i + 1)
+        lista.push(i + 1)
+        }
+    }
+
+    lista = lista.sort(comparador)
+
+    for (let z = 0; z < quantidade; z++){
+    elemento.innerHTML = elemento.innerHTML + `
+    <div class = "carta " data-identifier="card" onclick="clickTeste(this)">
+
+    <div class="carta-frente rodou">
+        <img src="imagens/${lista[z]}.gif" alt="" data-identifier="front-face">
+    </div>
+
+    <div class="carta-costa ">
+        <img src="imagens/front.png" alt="" data-identifier="back-face">
+    </div>
+
+    </div>
+    `;
+    }
+   
+
+    
+    console.log(lista)
+
+}
+
+cartasNaMesa()
